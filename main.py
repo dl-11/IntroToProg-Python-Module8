2a942fa3fd72f3a591ed57c57fac118e6e514795
DL <176932570+dl-11@users.noreply.github.com>
2024-09-20 15:09:27 -0700

    Add files via upload
    data classes module added and working

---
+++ main.py
@@ -0,0 +1,237 @@
+# ------------------------------------------------------------------------------------------------- #
+# Title: Assignment08-Starter files
+# # Description: A collection of classes for managing the application
+# ChangeLog: (Who, When, What)
+# David Levinson, 9/14/2024
+# ------------------------------------------------------------------------------------------------- #
+
+import json
+import data_classes as data
+
+
+class FileProcessor:
+    """
+    A collection of processing layer functions that work with Json files
+
+    ChangeLog: (Who, When, What)
+    David Levinson,9/20/2024,Created Class
+    """
+
+    @staticmethod
+    def read_employee_data_from_file(file_name: str, employee_data: list, employee_type: data.Employee):
+        """ This function reads data from a json file and loads it into a list of dictionary rows
+
+        ChangeLog: (Who, When, What)
+        David Levinson,9/20/2024,Created function
+
+        :param file_name: string data with name of file to read from
+        :param employee_data: list of dictionary rows to be filled with file data
+        :param employee_type: an reference to the Employee class
+        :return: list
+        """
+        try:
+            with open(file_name, "r") as file:
+                list_of_dictionary_data = json.load(file)  # the load function returns a list of dictionary rows.
+                for employee in list_of_dictionary_data:
+                    employee_object = employee_type()
+                    employee_object.first_name=employee["FirstName"]
+                    employee_object.last_name=employee["LastName"]
+                    employee_object.review_date=employee["ReviewDate"]
+                    employee_object.review_rating=employee["ReviewRating"]
+                    employee_data.append(employee_object)
+        except FileNotFoundError:
+            raise FileNotFoundError("Text file must exist before running this script!")
+        except Exception:
+            raise Exception("There was a non-specific error!")
+        return employee_data
+
+    @staticmethod
+    def write_employee_data_to_file(file_name: str, employee_data: list):
+        """ This function writes data to a json file with data from a list of dictionary rows
+
+        ChangeLog: (Who, When, What)
+        David Levinson,9/20/2024,Created function
+
+        :param file_name: string data with name of file to write to
+        :param employee_data: list of dictionary rows to be writen to the file
+
+        :return: None
+        """
+        try:
+            list_of_dictionary_data: list = []
+            for employee in employee_data:  # Convert List of employee objects to list of dictionary rows.
+                employee_json: dict = {"FirstName": employee.first_name,
+                                       "LastName": employee.last_name,
+                                       "ReviewDate": employee.review_date,
+                                       "ReviewRating": employee.review_rating
+                                       }
+                list_of_dictionary_data.append(employee_json)
+
+            with open(file_name, "w") as file:
+                json.dump(list_of_dictionary_data, file)
+        except TypeError:
+            raise TypeError("Please check that the data is a valid JSON format")
+        except PermissionError:
+            raise PermissionError("Please check the data file's read/write permission")
+        except Exception as e:
+            raise Exception("There was a non-specific error!")
+
+
+class IO:
+    """
+    A collection of presentation layer functions that manage user input and output
+
+    ChangeLog: (Who, When, What)
+    David Levinson,9/20/2024,Created Class
+    """
+    pass
+
+    @staticmethod
+    def output_error_messages(message: str, error: Exception = None):
+        """ This function displays the a custom error messages to the user
+
+        ChangeLog: (Who, When, What)
+        David Levinson,1.3.2030,Created function
+
+        :param message: string with message data to display
+        :param error: Exception object with technical message to display
+
+        :return: None
+        """
+
+        print(message, end="\n\n")
+        if error is not None:
+            print("-- Technical Error Message -- ")
+            print(error, error.__doc__, type(error), sep='\n')
+
+
+    @staticmethod
+    def output_menu(menu: str):
+        """ This function displays the menu of choices to the user
+
+        ChangeLog: (Who, When, What)
+        David Levinson,9/20/2024,Created function
+
+        :return: None
+        """
+        print()
+        print(menu)
+        print()
+
+
+    @staticmethod
+    def input_menu_choice():
+        """ This function gets a menu choice from the user
+
+        ChangeLog: (Who, When, What)
+        David Levinson,9/20/2024,Created function
+
+        :return: string with the users choice
+        """
+        choice = "0"
+        try:
+            choice = input("Enter your menu choice number: ")
+            if choice not in ("1", "2", "3", "4"):  # Note these are strings
+                raise Exception("Please, choose only 1, 2, 3, or 4")
+        except Exception as e:
+            IO.output_error_messages(e.__str__())  # passing the exception object to avoid the technical message
+
+        return choice
+
+
+    @staticmethod
+    def output_employee_data(employee_data: list):
+        """ This function displays employee data to the user
+
+        ChangeLog: (Who, When, What)
+        David Levinson,9/20/2024,Created function
+
+        :param employee_data: list of employee object data to be displayed
+
+        :return: None
+        """
+        message:str = ''
+        print()
+        print("-" * 50)
+        for employee in employee_data:
+            if employee.review_rating == 5:
+                message = " {} {} is rated as 5 (Leading)"
+            elif employee.review_rating == 4:
+                message = " {} {} is rated as 4 (Strong)"
+            elif employee.review_rating == 3:
+                message = " {} {} is rated as 3 (Solid)"
+            elif employee.review_rating == 2:
+                message = " {} {} is rated as 2 (Building)"
+            elif employee.review_rating == 1:
+                message = " {} {} is rated as 1 (Not Meeting Expectations)"
+
+            print(message.format(employee.first_name, employee.last_name, employee.review_date, employee.review_rating))
+        print("-" * 50)
+        print()
+
+
+    @staticmethod
+    def input_employee_data(employee_data: list, employee_type: data.Employee):
+        """ This function gets the first name, last name, and GPA from the user
+
+        ChangeLog: (Who, When, What)
+        David Levinson,9/20/2024,Created function
+
+        :param employee_data: list of dictionary rows to be filled with input data
+
+        :return: list
+        """
+
+        try:
+            # Input the data
+            employee_object = employee_type()
+            employee_object.first_name = input("What is the employee's first name? ")
+            employee_object.last_name = input("What is the employee's last name? ")
+            employee_object.review_date = input("What is their review date? ")
+            employee_object.review_rating = int(input("What is their review rating? "))
+            employee_data.append(employee_object)
+
+        except ValueError as e:
+            IO.output_error_messages("That value is not the correct type of data!", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+
+        return employee_data
+
+
+# Beginning of the main body of this script
+employees = FileProcessor.read_employee_data_from_file(file_name=data.FILE_NAME,
+                                                       employee_data=data.employees,
+                                                       employee_type=data.Employee)  # Note this is the class name (ignore the warning)
+
+# Repeat the follow tasks
+while True:
+    IO.output_menu(menu=data.MENU)
+
+    menu_choice = IO.input_menu_choice()
+
+    if menu_choice == "1":  # Display current data
+        try:
+            IO.output_employee_data(employee_data=employees)
+        except Exception as e:
+            IO.output_error_messages(e)
+        continue
+
+    elif menu_choice == "2":  # Get new data (and display the change)
+        try:
+            employees = IO.input_employee_data(employee_data=employees, employee_type=data.Employee)  # Note this is the class name (ignore the warning)
+            IO.output_employee_data(employee_data=employees)
+        except Exception as e:
+            IO.output_error_messages(e)
+        continue
+
+    elif menu_choice == "3":  # Save data in a file
+        try:
+            FileProcessor.write_employee_data_to_file(file_name=data.FILE_NAME, employee_data=employees)
+            print(f"Data was saved to the {data.FILE_NAME} file.")
+        except Exception as e:
+            IO.output_error_messages(e)
+        continue
+
+    elif menu_choice == "4":  # End the program
+        break  # out of the while loop
