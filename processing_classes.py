e41108e20d601b8aaaeaca33c97509a90372e0c7
DL <176932570+dl-11@users.noreply.github.com>
2024-09-24 18:03:12 -0700

    Add files via upload
    updated descriptions

--- processing_classes.py
+++ processing_classes.py
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------- #
 # Title: Application Classes Module
-# # Description: A collection of processing classes for managing the application
+# Description: A collection of processing classes for managing the application
 # ChangeLog: (Who, When, What)
 # David Levinson,9/20/2024,Created Processing Classes Module
 # ------------------------------------------------------------------------------- #
@@ -16,7 +16,7 @@ except Exception as e:
 
 class FileProcessor:
     """
-    A collection of processing layer functions that work with Json files
+    A collection of processing layer functions that work with JSON files
 
     ChangeLog: (Who, When, What)
     David Levinson,9/20/2024,Created Class
@@ -24,59 +24,58 @@ class FileProcessor:
 
     @staticmethod
     def read_employee_data_from_file(file_name: str, employee_data: list, employee_type: data.Employee):
-        """ This function reads data from a json file and loads it into a list of dictionary rows
+        """ This function reads data from a JSON file and loads it into a list of employee objects
 
         ChangeLog: (Who, When, What)
         David Levinson,9/20/2024,Created function
 
         :param file_name: string data with name of file to read from
-        :param employee_data: list of dictionary rows to be filled with file data
-        :param employee_type: an reference to the Employee class
-        :return: list
+        :param employee_data: list to be filled with employee objects
+        :param employee_type: a reference to the Employee class
+        :return: list of employee objects
         """
         try:
             with open(file_name, "r") as file:
                 list_of_dictionary_data = json.load(file)  # the load function returns a list of dictionary rows.
                 for employee in list_of_dictionary_data:
                     employee_object = employee_type()
-                    employee_object.first_name=employee["FirstName"]
-                    employee_object.last_name=employee["LastName"]
-                    employee_object.review_date=employee["ReviewDate"]
-                    employee_object.review_rating=employee["ReviewRating"]
+                    employee_object.first_name = employee["FirstName"]
+                    employee_object.last_name = employee["LastName"]
+                    employee_object.review_date = employee["ReviewDate"]
+                    employee_object.review_rating = employee["ReviewRating"]
                     employee_data.append(employee_object)
-        except FileNotFoundError:
-            raise FileNotFoundError("Text file must exist before running this script!")
-        except Exception:
-            raise Exception("There was a non-specific error!")
+        except FileNotFoundError as e:
+            print(f"Error: {file_name} not found. {e}")
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON from {file_name}. {e}")
+        except Exception as e:
+            print(f"An error occurred while reading from {file_name}. {e}")
         return employee_data
 
     @staticmethod
     def write_employee_data_to_file(file_name: str, employee_data: list):
-        """ This function writes data to a json file with data from a list of dictionary rows
+        """ This function writes data to a JSON file from a list of employee objects
 
         ChangeLog: (Who, When, What)
         David Levinson,9/20/2024,Created function
 
         :param file_name: string data with name of file to write to
-        :param employee_data: list of dictionary rows to be writen to the file
-
+        :param employee_data: list of employee objects to be written to the file
         :return: None
         """
         try:
-            list_of_dictionary_data: list = []
-            for employee in employee_data:  # Convert List of employee objects to list of dictionary rows.
-                employee_json: dict = {"FirstName": employee.first_name,
-                                       "LastName": employee.last_name,
-                                       "ReviewDate": employee.review_date,
-                                       "ReviewRating": employee.review_rating
-                                       }
-                list_of_dictionary_data.append(employee_json)
-
+            list_of_dictionary_data = []
+            for employee in employee_data:
+                employee_dict = {
+                    "FirstName": employee.first_name,
+                    "LastName": employee.last_name,
+                    "ReviewDate": employee.review_date,
+                    "ReviewRating": employee.review_rating
+                }
+                list_of_dictionary_data.append(employee_dict)
             with open(file_name, "w") as file:
                 json.dump(list_of_dictionary_data, file)
-        except TypeError:
-            raise TypeError("Please check that the data is a valid JSON format")
-        except PermissionError:
-            raise PermissionError("Please check the data file's read/write permission")
+        except TypeError as e:
+            print(f"Error: Data type issue. {e}")
         except Exception as e:
-            raise Exception("There was a non-specific error!")
+            print(f"An error occurred while writing to {file_name}. {e}")
